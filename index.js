888a326b8512676f2b2ad6e24ee2b9335a2fdaa3
OdieStar <orphisia@gmail.com>
2026-01-01 15:01:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ const DiscordBot = require('./bot');
 const createWebServer = require('./server');
 const ProgressTracker = require('./progressTracker');
 
+function ValidateEnv(){
+    const required = [
+        'DISCORD_BOT_TOKEN',
+        'DISCORD_CLIENT_ID',
+        'STEPIK_CLIENT_ID',
+        'STEPIK_CLIENT_SECRET',
+        'BASE_URL'
+      ];
+
+      const missing = required.filter(key => !process.env[key]);
+
+      if (missing.length > 0) {
+        console.error('Missing required env variables:');
+        missing.forEach(key => console.error(`   - ${key}`));
+        console.error('\ check .env file');
+        process.exit(1);
+      }
+
+      const recommended = [
+        'DISCORD_GUILD_ID',
+        'ROLE_CHAPTER_1',
+        'ROLE_CHAPTER_2',
+        'ROLE_CHAPTER_3',
+        'ROLE_COURSE_COMPLETE'
+      ];
+
+      //Add more roles later
+    
+}
+
+async function main(){
+    console.log("Starting Cogniterra bot");
+
+    validateEnv(); 
+
+    initializeDatabase();
+
+    cleanupExpiredTokens();
+}
